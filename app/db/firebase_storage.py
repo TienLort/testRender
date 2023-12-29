40d7b8bb553216c0b434c793edca6089e486d088
TienLort <vantien281201@gmail.com>
2023-12-29 18:58:53 +0700

    update 2

--- app/db/firebase_storage.py
+++ app/db/firebase_storage.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
 
-cred = credentials.Certificate("D:\D\suong\\app\db\credential.json")
+cred = credentials.Certificate("../db/credential.json")
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'capstoneproject-ca420.appspot.com'
 })
